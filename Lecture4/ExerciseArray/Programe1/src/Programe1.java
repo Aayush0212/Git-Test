12c68b9964af64cdffe0f0254bc5538ce549ed65
Aayush0212 <aayush18csu004@ncuindia.edu>
2019-01-16 09:17:37 +0530

    Added files

--- Lecture4/ExerciseArray/Programe1/src/Programe1.java
+++ Lecture4/ExerciseArray/Programe1/src/Programe1.java
@@ -1,26 +1,55 @@
 import java.util.*;
-class Programe1{
-	public static void main(String[] args) {
+public class Programe1
+{
+	public static void main (String[]args)
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the 2-D array (4x4) (each element must be of two digits)");
+		int arr [][]=new int[4][4];
+		for (int i=0;i<4;i++)
 		{
-			int counter;	
-			Scanner scan = new Scanner(System.in);
-		    int elements[]=new int[5];
-		    System.out.println("Enter 5 elements in the array");
-		    for (counter=0;counter<5;counter++)
-		    {
-			    elements[counter]=scan.nextInt();
-		    
-		    }
-			
-			if(elements[counter]>99){
-				System.out.println("Enter a two digit number");
-				break;
+			for (int j=0; j<4;j++)
+			{
+				arr[i][j] = scan.nextInt();
+				while(true)
+				{
+					if (arr[i][j]>9 && arr[i][j]<100)
+						break;
+					else
+					{
+						System.out.println("Re-enter the element (TWO DIGIT)");
+						arr[i][j] = scan.nextInt();
+					}
+				}
 			}
-			else
-				System.out.println("The element are "+ elements[counter]);
-
-
-	
+		}
+		System.out.println("The entered 2-D array in matrix form is:");
+		for (int i=0;i<4;i++)
+		{
+			for (int j=0; j<4;j++)
+			{
+				System.out.print(arr[i][j]+"\t");
+			}
+		System.out.println("");
+		}
+		for (int i=0;i<4;i++)
+		{
+			for (int j=0;j<4;j++)
+			{
+				if (arr[i][j]%2==1)
+					arr[i][j]*=2;
+				else
+					continue;
+			}
+		}
+		System.out.println("Array after being operated is:");
+		for (int i=0;i<4;i++)
+		{
+			for (int j=0; j<4;j++)
+			{
+				System.out.print(arr[i][j]+"\t");
+			}
+		System.out.println("");
 		}
 	}
 }
